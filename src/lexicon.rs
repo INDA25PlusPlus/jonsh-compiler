f3e4edc38a985dff3af9fbf8c456a77da2210f23
Jonathan Hahne <jonathanhahne@c-83-226-241-91.bbcust.telenor.se>
2025-10-31 00:18:01 +0100

    Finally finished tokenizer

--- src/lexicon.rs
+++ src/lexicon.rs
@@ -1,1 +1,43 @@
-
+pub fn tokenize(input: &String) -> Vec<String> {
+    let mut tokens = Vec::new();
+    let mut current_token = String::new();
+    for c in input.chars() {
+        if [
+            "+", "-", "*", "/", "=", "<", ">", "!", "(", ")", "{", "}", ",", ";",
+        ]
+        .contains(&c.to_string().as_str())
+        {
+            if !current_token.is_empty() {
+                tokens.push(current_token.clone());
+                current_token.clear();
+            }
+            tokens.push(c.to_string());
+        } else if c.is_whitespace() {
+            if !current_token.is_empty() {
+                tokens.push(current_token.clone());
+                current_token.clear();
+            }
+        } else {
+            current_token.push(c);
+        }
+    }
+    if !current_token.is_empty() {
+        tokens.push(current_token);
+    }
+    for token in &mut tokens {
+        if ["return", "if", "else", "elif", "while", "for"].contains(&token.as_str()) {
+            *token = format!("Keyword({})", token);
+        } else if token.parse::<i32>().is_ok() {
+            *token = format!("Number({})", token);
+        } else if ["+", "-", "*", "/", "=", "<", ">", "<=", ">=", "!"].contains(&token.as_str()) {
+            *token = format!("Operator({})", token);
+        } else if ["(", ")", "{", "}", ","].contains(&token.as_str()) {
+            *token = format!("Symbol({})", token);
+        } else if token == ";" {
+            *token = "Terminator".to_string();
+        } else {
+            *token = format!("Identifier({})", token);
+        }
+    }
+    tokens
+}
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,16 @@
-use io::stdin;
-use std::io;
+use std::env;
+use std::fs;
 
 mod lexicon;
-fn main() {}
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    let file_path = &args[1];
+    let mut contents =
+        fs::read_to_string(file_path).expect("Shoyld have been able to read the file");
+    contents = contents.replace("\n", "").replace("\t", "");
+    let contents: String = contents.split_whitespace().collect();
+    let tokens: String = lexicon::tokenize(&contents).join(", ");
+
+    println!("{:?}", tokens);
+}
